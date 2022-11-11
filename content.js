4770a713b792d9d424f1ef2e851eea8bff6cb664
shitamori1272 <33188869+shitamori1272@users.noreply.github.com>
2022-11-11 01:56:29 -0700

    add content.js

---
+++ content.js
@@ -0,0 +1,41 @@
+// Initialize button with user's preferred color
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
+let timeColumns = document.querySelector(".time")
+for(var i=0; i<timeColumns.childElementCount; i++ ) {
+  var timeElement = timeColumns.children[i].firstChild;
+  var timeText = timeElement.textContent;
+  timeElement.textContent = timeElement.textContent + getLocalTimezondeDateString(timeText.replace("-", ""))
+}
+
+function getJSTCurrentDateString() {
+  var currentDate = new Date(Date.now());
+  options = {
+    year: 'numeric', month: 'numeric', day: 'numeric',
+    hour: 'numeric', minute: 'numeric',
+    hour12: false,
+    timeZone: 'Asia/Tokyo'
+  };
+  return new Intl.DateTimeFormat('ja-JP', options).format(currentDate).split(" ")[0];
+}
+
+function getLocalTimezondeDateString(jstHHMMString) {
+  var [hour, minute] = jstHHMMString.split(":");
+  [year, month, day] = getJSTCurrentDateString().split("/");
+  var date = new Date(Date.UTC(+year, +month, +day, +hour, +minute));
+  // JST -> UTC
+  date.setTime(date.getTime() - (9 * 60 * 60 * 1000));
+  // UTC -> CANADA
+  // date.setTime(date.getTime() - (7 * 60 * 60 * 1000));
+  options = {
+    hour: 'numeric', minute: 'numeric',
+    hour12: false,
+    timeZone: 'MST'
+  };
+  return "(" + new Intl.DateTimeFormat('ja-JP', options).format(date) + "-)";
+}
+
